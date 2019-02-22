1a856dfaad462f95a582d520aa94152c3b14a48c
genkilife <genkilifegogo@gmail.com>
2019-02-22 00:28:18 -0800

    finish generate ramdom placed images

--- util/data_augment.py
+++ util/data_augment.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from pathlib import Path
 import numpy as np
+import random
 
 
 ## Read data from UCI 2014 BW image files
@@ -42,12 +43,34 @@ for file_name in os.listdir(path_to_original_dir):
         if len([x for x in range(0, xBound) if img_orig.getpixel((x, y_max))!=0]) != 0:
             break
     
-    print(", ".join(str(_) for _ in [x_min, x_max, y_min, y_max]))        
+    print(file_name + " " + ", ".join(str(_) for _ in [x_min, x_max, y_min, y_max]))
     
-    img_orig.show()
-    
-    print()
-    
+    # Assert no empty images in the dataset
+    assert(x_min <= x_max)
+    assert(y_min <= y_max)
 
+    x_img_len = x_max - x_min + 1
+    y_img_len = y_max - y_min + 1
+
+    x_reg_len = xBound - x_img_len
+    y_reg_len = yBound - y_img_len
+
+    img_content = img_orig.copy()
+    img_content = img_content.crop(box=[x_min, y_min, x_max, y_max])
+
+    img_content.show()
+    
+    for idx in range(0, 4):
+        x_index = random.randrange(x_reg_len)
+        y_index = random.randrange(y_reg_len)
+        assert(x_index < x_reg_len)
+        assert(y_index < y_reg_len)
+
+        # create black new image
+        img_displaced = Image.new('L', img_orig.size, color='black')
+
+        # paste the content images
+        img_displaced.paste(img_content, box=[x_index, y_index])
+        #img_displaced.show()
 
 print("finish")
