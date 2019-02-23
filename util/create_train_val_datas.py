dadb968a4fc10a0622547825683778f360496879
genkilife <genkilifegogo@gmail.com>
2019-02-23 01:12:18 -0800

    scale down images

--- util/create_train_val_datas.py
+++ util/create_train_val_datas.py
@@ -29,26 +29,31 @@ for species_name in os.listdir(path_to_GREY_MASKED_dir):  # assuming gif
     assert(len(file_list)>=train_case_num>=0)
     assert(len(file_list)>=test_case_num>=0)
 
-    try:
-        train_species_dir_path = "/".join([path_to_TRAIN_dir, species_name])
-        if os.path.isdir(train_species_dir_path) is not True:
-            os.makedirs(train_species_dir_path, exist_ok=True)
+    train_species_dir_path = "/".join([path_to_TRAIN_dir, species_name])
+    if os.path.isdir(train_species_dir_path) is not True:
+        os.makedirs(train_species_dir_path, exist_ok=True)
 
-        test_species_dir_path   = "/".join([path_to_TEST_dir, species_name])
-        if os.path.isdir(test_species_dir_path) is not True:
-            os.makedirs(test_species_dir_path, exist_ok=True)
+    test_species_dir_path   = "/".join([path_to_TEST_dir, species_name])
+    if os.path.isdir(test_species_dir_path) is not True:
+        os.makedirs(test_species_dir_path, exist_ok=True)
 
-        for train_file_name in file_list[0:train_case_num]:
-            src_train_file_path = "/".join([path_to_GREY_MASKED_dir, species_name, train_file_name])
-            dst_train_file_path = "/".join([path_to_TRAIN_dir      , species_name, train_file_name])
-            copyfile(src_train_file_path, dst_train_file_path)
+    maxsize = (360, 480)
 
-        for test_file_name in file_list[train_case_num:]:
-            src_test_file_path = "/".join([path_to_GREY_MASKED_dir, species_name, test_file_name])
-            dst_test_file_path = "/".join([path_to_TEST_dir,  species_name, test_file_name])
-            copyfile(src_test_file_path, dst_test_file_path)
-    except Exception as e:
-        print(str(e))
+    for train_file_name in file_list[0:train_case_num]:
+        src_train_file_path = "/".join([path_to_GREY_MASKED_dir, species_name, train_file_name])
+        dst_train_file_path = "/".join([path_to_TRAIN_dir      , species_name, train_file_name])
 
+        img = Image.open(src_train_file_path)
+        assert(img is not None)
+        img.thumbnail(maxsize, Image.ANTIALIAS)
+        img.save(dst_train_file_path)
 
+        #copyfile(src_train_file_path, dst_train_file_path)
 
+    for test_file_name in file_list[train_case_num:]:
+        src_test_file_path = "/".join([path_to_GREY_MASKED_dir, species_name, test_file_name])
+        dst_test_file_path = "/".join([path_to_TEST_dir,  species_name, test_file_name])
+
+        img = Image.open(src_test_file_path)
+        img.thumbnail(maxsize, Image.ANTIALIAS)
+        img.save(dst_test_file_path)
