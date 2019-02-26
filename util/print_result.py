037acbf538fb5e657bdc2ad83d815426ff85680d
Yen-Kai Wang <yenkaiw1@uci.edu>
2019-02-26 12:50:37 -0800

    dump image file

--- util/print_result.py
+++ util/print_result.py
@@ -16,7 +16,12 @@ from PIL import Image
 
 
 train_acc = pickle.load(open("GREY_NON_MASKED_30species.train_acc.log", 'rb'))
+train_mask_acc = pickle.load(open("GREY_MASKED.train_acc.log", 'rb'))
+train_mask_aug_acc = pickle.load(open("MASK_GREY_AUMENT.train_acc.log", 'rb'))
+
 val_acc = pickle.load(open("GREY_NON_MASKED_30species.val_acc.log", 'rb'))
+val_mask_acc = pickle.load(open("GREY_MASKED.val_acc.log", 'rb'))
+val_mask_aug_acc = pickle.load(open("MASK_GREY_AUMENT.val_acc.log", 'rb'))
 
 print(train_acc)
 print(val_acc)
@@ -24,6 +29,10 @@ print(val_acc)
 
 plt.plot(train_acc)
 plt.plot(val_acc)
+plt.plot(train_mask_acc)
+plt.plot(val_mask_acc)
+plt.plot(train_mask_aug_acc)
+plt.plot(val_mask_aug_acc)
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
